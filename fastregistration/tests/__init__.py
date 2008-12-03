a61536483d2c03ba867311b10360bbd891cb0b15
Kegan Gan <kegan@kebook.local>
2008-12-03 16:43:31 +0800

    Disabled TEMPLATE_DIRS to ensure does not affect independent app testing.

--- fastregistration/tests/__init__.py
+++ fastregistration/tests/__init__.py
@@ -2,3 +2,8 @@ from registration_tests import *
 from password_tests import *
 from profile_tests import *
 from login_logout_tests import *
+
+
+# Disabled TEMPLATE_DIRS so that custom templates would not intefere with tests.
+from django.conf import settings
+settings.TEMPLATE_DIRS = ()
