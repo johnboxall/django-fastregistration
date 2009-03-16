3cc3794f5fb439a6d477532815d208ed33f4f009
John Boxall <john@handimobility.ca>
2009-03-16 13:35:10 -0700

    Very fast registration : )

--- fastregistration/urls.py
+++ fastregistration/urls.py
@@ -1,41 +1,63 @@
 from django.conf.urls.defaults import *
-from django.contrib.auth import views as auth_views
-from django.views.generic.simple import direct_to_template
-
-from fastregistration import views as fast_views
 
 
-urlpatterns = patterns('',
-    # register
-    url(r'^register/$', fast_views.register,
-        name='fast-register'),
-    url(r'^register/done/$', direct_to_template, {'template': 'registration/register_done.html'},
-        name='fast-register-done'),
-
-    # forget password
-    url(r'^password_request/$', auth_views.password_reset, {'template_name': 'registration/password_request.html'},
-        name='fast-password-request'),
-    url(r'^password_request/done/$', auth_views.password_reset_done, {'template_name': 'registration/password_request_done.html'},
-        name='fast-password-request-done'),
-    url(r'^password_reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, {'template_name': 'registration/password_reset.html'},
-        name='fast-password-reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_complete, {'template_name': 'registration/password_reset_done.html'},
-        name='fast-password-reset-done'),
-        
-    # manage profile email and password
-    url(r'^profile/$', fast_views.profile,
-        name='fast-profile'),
-    url(r'^profile/email/update/$', fast_views.profile_email_update,
-        name='fast-profile-email-update'),
-    url(r'^profile/password/update/$', fast_views.profile_password_update,
-        name='fast-profile-password-update'),
-        
-    # login logout
-    url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'},
-        name='fast-login'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'registration/logout.html'},
-        name='fast-logout'),
-    url(r'^logout_then_login/$', auth_views.logout_then_login,
-        name='fast-logout-then-login'),
-    
+urlpatterns = patterns("fastregistration.views",
+	url(r'^register/$', "register", name="register"),
 )
+
+urlpatterns += patterns('django.contrib.auth.views',
+
+    url(r'^password/reset/$', "password_reset", name='auth_password_reset'),
+    url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 
+         "password_reset_confirm", name='auth_password_reset_confirm'),
+    url(r'^password/reset/complete/$', "password_reset_complete", name='auth_password_reset_complete'),
+    url(r'^password/reset/done/$', "password_reset_done", name='auth_password_reset_done'),
+
+
+#     url(r'^logout/$', 'logout_then_login', name="logout"),
+#     url(r'^login/$', 'login', name="login"),
+)
+
+
+
+
+# from django.conf.urls.defaults import *
+# from django.contrib.auth import views as auth_views
+# from django.views.generic.simple import direct_to_template
+# 
+# from fastregistration import views as fast_views
+# 
+# 
+# urlpatterns = patterns('',
+#     # register
+#     url(r'^register/$', fast_views.register,
+#         name='fast-register'),
+# 
+#     # forget password
+#     url(r'^password_request/$', auth_views.password_reset, {'template_name': 'registration/password_request.html'},
+#         name='fast-password-request'),
+#     url(r'^password_request/done/$', auth_views.password_reset_done, {'template_name': 'registration/password_request_done.html'},
+#         name='fast-password-request-done'),
+#     url(r'^password_reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, {'template_name': 'registration/password_reset.html'},
+#         name='fast-password-reset'),
+#     url(r'^password_reset/done/$', auth_views.password_reset_complete, {'template_name': 'registration/password_reset_done.html'},
+#         name='fast-password-reset-done'),
+#         
+#     # manage profile email and password
+#     url(r'^profile/$', fast_views.profile,
+#         name='fast-profile'),
+#     url(r'^profile/email/update/$', fast_views.profile_email_update,
+#         name='fast-profile-email-update'),
+#     url(r'^profile/password/update/$', fast_views.profile_password_update,
+#         name='fast-profile-password-update'),
+#         
+#     # login logout
+#     url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'},
+#         name='fast-login'),
+#     url(r'^logout/$', auth_views.logout, {'template_name': 'registration/logout.html'},
+#         name='fast-logout'),
+#     url(r'^logout_then_login/$', auth_views.logout_then_login,
+#         name='fast-logout-then-login'),
+#     
+# )
+
--- fastregistration/views.py
+++ fastregistration/views.py
@@ -1,104 +1,35 @@
+from django.conf import settings
 from django.contrib import auth
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.utils.translation import ugettext as _
-    
-from forms import *
 
+from fastregistration.forms import RegistrationForm
 
-def register(request, success_url=None, form_class=RegistrationForm,
-            profile_callback=None,
-            template_name='registration/register.html'):
+def register(request, next=settings.LOGIN_REDIRECT_URL, form_cls=RegistrationForm,
+             profile_cb=None, login_cb=None, template='registration/register.html',
+             extra_context=None):
     """
-    Register new users.
+    Register user by e-mail address and log them in.
+    
     """
     if request.method == 'POST':
-        form = form_class(request.POST)
+        form = form_cls(request.POST)
         if form.is_valid():
-            # Create the user instance.
-            user = User.objects.create_user(form.cleaned_data["email1"], form.cleaned_data["email1"], form.cleaned_data["password1"])
-            
-            if profile_callback is not None:
-                profile_callback(user=user)
-            
-            # Login.
-            user = auth.authenticate(username=user.username, password=form.cleaned_data["password1"])
+            user, pw = form.save(request, profile_cb)
+            user = auth.authenticate(username=user.username, password=pw)
             auth.login(request, user)
-            
-            # Show and let user edit profile upon registration.
-            return HttpResponseRedirect(success_url or reverse('fast-register-done'))
-    elif request.method == 'GET':
-        form = form_class()
+            response = HttpResponseRedirect(next)
+            if login_cb is not None:
+                response = login_cb(response, user)
+            return response
     else:
-        return HttpResponseRedirect(request.path)
+        form = form_cls()
+    context = {'form': form}
+    if extra_context is not None:
+        context = context.update(extra_context)
+    return render_to_response(template, context, RequestContext(request))
     
-    context = RequestContext(request, {
-        'form': form
-    })
-    return render_to_response(template_name, context)
-    
-@login_required
-def profile(request, template_name='registration/profile.html'):
-    """
-    Display user profile information.
-    """
-    if request.method == 'GET':
-        pass
-    else:
-        return HttpResponseRedirect(reverse('fast-profile'))
-        
-    context = RequestContext(request)
-    return render_to_response(template_name, context)
-    
-@login_required
-def profile_email_update(request, form_class=ProfileEmailForm,
-            success_message=_('Your email has been successfully updated.'),
-            template_name='registration/profile_email_update.html'):
-    """
-    Update user profile email.
-    """
-    if request.method == 'POST':
-        form = form_class(request.user, request.POST)
-        if form.is_valid():
-            request.user.username = form.cleaned_data['email1']
-            request.user.email = form.cleaned_data['email1']
-            request.user.save()
-            request.user.message_set.create(message=success_message)
-            return HttpResponseRedirect(reverse('fast-profile'))
-    elif request.method == 'GET':
-        form = form_class(request.user)
-    else:
-        return HttpResponseRedirect(request.path)
 
-    context = RequestContext(request, {
-        'form': form
-    })
-    return render_to_response(template_name, context)
 
-@login_required
-def profile_password_update(request, form_class=ProfilePasswordForm,
-            success_message=_('Your password has been successfully updated.'),
-            template_name='registration/profile_password_update.html'):
-    """
-    Update user profile password.
-    """
-    if request.method == 'POST':
-        form = form_class(request.user, request.POST)
-        if form.is_valid():
-            request.user.set_password(form.cleaned_data['password1'])
-            request.user.save()
-            request.user.message_set.create(message=success_message)
-            return HttpResponseRedirect(reverse('fast-profile'))
-    elif request.method == 'GET':
-        form = form_class(request.user)
-    else:
-        return HttpResponseRedirect(request.path)
-
-    context = RequestContext(request, {
-        'form': form
-    })
-    return render_to_response(template_name, context)
