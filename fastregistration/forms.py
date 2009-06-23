e609cc541ef73d352aeff3e359461b03d299d6e2
John Boxall <john@handimobility.ca>
2009-06-23 11:19:40 -0700

    Updated for our username max length...

--- fastregistration/forms.py
+++ fastregistration/forms.py
@@ -8,7 +8,7 @@ class RegistrationForm(forms.Form):
     Registration form for new users.
     
     """
-    email     = forms.EmailField(label=_("Email"), max_length=30)  # username max_length = 30
+    email     = forms.EmailField(label=_("Email"), max_length=75)  # username max_length = 30
     password1 = forms.CharField(label=_("Password"), max_length=16, widget=forms.PasswordInput)
     password2 = forms.CharField(label=_("Password Confirmation"), max_length=16, widget=forms.PasswordInput)
     
@@ -23,11 +23,12 @@ class RegistrationForm(forms.Form):
     def clean(self):
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-                raise forms.ValidationError(_(u'You must type the same password each time'))
+                raise forms.ValidationError(_(u'You must type the same password each time'))                
         return self.cleaned_data
 
+    # @@@ This business is really silly. No profile_cb - do that yourself with signals.
+    # @@@ This should be made into a request form.
     def save(self, request, profile_cb=None):
-        
         args = [self.cleaned_data["email"], self.cleaned_data["email"], self.cleaned_data["password1"]]
         user = User.objects.create_user(*args)
         
